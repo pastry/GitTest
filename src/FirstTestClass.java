cceb739f9615e0718999eb23a84a6f29c6e140b1
jack <jack@pastryness>
2010-09-04 20:11:20 +0200

    Modifying an exitisting class.
    Signed-off-by: jack <jack@pastryness>

--- src/FirstTestClass.java
+++ src/FirstTestClass.java
@@ -1,4 +1,13 @@
 
 public class FirstTestClass {
-
+	
+	private String var;
+	
+	public FirstTestClass() {
+		
+		super();
+		
+		var = new String("Initializing the fucking var string!");
+		
+	}
 }
